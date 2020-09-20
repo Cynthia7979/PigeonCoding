40d5ed136959d1d8c42bc86f0c3f21298c6b369d
BuildTools <CynthiaWang7979@hotmail.com>
2020-09-20 19:52:37 +0800

    R e n a i s s a n c e

---
+++ Classes/[0]/hello_more_world.py
@@ -0,0 +1,12 @@
+message = 'Hello'
+person = "Pigeon Coding"
+time = 0
+
+while True:
+    print('>', message, person, '!')
+    print('====='+str(time)+'=====')
+    print()
+    message = input('Enter message: ')
+    person = input('Enter person: ')
+    time += 1
+    # time = time + 1
---
+++ Classes/[1]/spaceship_control.py
@@ -0,0 +1,10 @@
+destination = 'Earth'
+status = 'Arrived'
+
+
+print("""
+WELCOME TO PIGEON SPACECRAFT CONTROL PANEL
+===========================================
+""")
+
+while True:
