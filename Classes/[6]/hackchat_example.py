64c8aacac88b30958480651b88bc2e438ba70ace
BuildTools <CynthiaWang7979@hotmail.com>
2020-12-23 21:31:10 +0800

    hackchat bot example update

--- Classes/[6]/hackchat_example.py
+++ Classes/[6]/hackchat_example.py
@@ -1,12 +1,17 @@
 import hackchat
+from random import randint
 
 chat = hackchat.HackChat('CynthiaBot', 'pigeons')
 
 
-def try_reply(chat_, message, sender):
+def try_reply(chat_:hackchat.HackChat, message, sender):
     print(f'{message} by {sender}')
-    if message == '/help':
+    if message == '!help':
         chat_.send_message('Unknown command. Type "/help" for help.')
+    if message == '!changeyourcolor':
+        color = format(randint(0, 255), 'x')+format(randint(0, 255), 'x')+format(randint(0, 255), 'x')
+        print(color)
+        chat_._send_packet({'cmd': 'changecolor', 'color': color})
 
 
 def welcome(chat_, username):
@@ -20,4 +25,5 @@ def bye(chat_, username):
 chat.on_message += [try_reply]
 chat.on_join += [welcome]
 chat.on_leave += [bye]
+chat.send_message('/help')
 chat.run()
