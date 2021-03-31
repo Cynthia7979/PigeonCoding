f4e1c7245e3da44799293ce2d6f0307e6065a269
BuildTools <CynthiaWang7979@hotmail.com>
2021-03-31 18:07:52 +0800

    Updates to events

--- Classes/[12]/events.py
+++ Classes/[12]/events.py
@@ -5,8 +5,10 @@ from pygame.locals import *
 
 def main():
     WIDTH, HEIGHT = 1000, 600
-    XSPEED = 10
-    YSPEED = 10
+    XSTEP = 10
+    YSTEP = 10
+    xspeed = 0
+    yspeed = 0
 
     pygame.init()
     display = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -19,20 +21,27 @@ def main():
     while True:  # main loop
         display.fill((255, 255, 255))
         display.blit(ball_surf, ball_rect)
+        ball_rect.x += xspeed
+        ball_rect.y += yspeed
         for event in pygame.event.get():
             if event.type == QUIT:
                 terminate()
+            elif event.type == KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    xspeed = XSTEP
+                elif event.key == pygame.K_LEFT:
+                    xspeed = -XSTEP
+                elif event.key == pygame.K_UP:
+                    yspeed = -YSTEP
+                elif event.key == pygame.K_DOWN:
+                    yspeed = YSTEP
             elif event.type == KEYUP:
                 if event.key == pygame.K_q:
                     terminate()
-                elif event.key == pygame.K_RIGHT:
-                    ball_rect.x += XSPEED
-                elif event.key == pygame.K_LEFT:
-                    ball_rect.x -= XSPEED
-                elif event.key == pygame.K_UP:
-                    ball_rect.y -= YSPEED
-                elif event.key == pygame.K_DOWN:
-                    ball_rect.y += YSPEED
+                elif event.key in (pygame.K_RIGHT, pygame.K_LEFT):
+                    xspeed = 0
+                elif event.key in (pygame.K_UP, pygame.K_DOWN):
+                    yspeed = 0
 
         pygame.display.flip()
         clock.tick(30)
